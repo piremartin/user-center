b47331e956638c928caf6738dec4bae32e06e835
haojay <haojayc@163.com>
2018-12-12 16:38:09 +0800

    init

---
+++ src/main/java/com/chj/usercenter/domain/UserE.java
@@ -0,0 +1,26 @@
+package com.chj.usercenter.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "user")
+public class UserE implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    @Column(name = "create_time")
+    private LocalDateTime createTime;
+
+
+}
